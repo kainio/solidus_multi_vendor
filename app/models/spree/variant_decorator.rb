ee5d6f03b8a2a76d5ed0612f2fc65aaeb333e8a1
Todd Blanchard <tblanchard@mac.com>
2018-04-07 16:18:05 -0700

    fixed missing comma

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Variant.class_eval do
 
-  delegate :vendor_id to: :product
+  delegate :vendor_id, to: :product
 
   def create_stock_items
     Spree::StockLocation.where(propagate_all_variants: true, vendor_id: vendor_id).each do |stock_location|
