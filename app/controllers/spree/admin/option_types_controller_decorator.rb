85ef9253927805fb1e92e04b5e24be63b23b2bb1
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2018-04-24 14:48:39 -0500

    Remove unnecessary files

--- app/controllers/spree/admin/option_types_controller_decorator.rb
+++
@@ -1,3 +0,0 @@
-Spree::Admin::OptionTypesController.class_eval do
-  before_action :set_vendor_id, only: [:create, :update]
-end
--- app/controllers/spree/admin/variants_controller_decorator.rb
+++
@@ -1,3 +0,0 @@
-Spree::Admin::VariantsController.class_eval do
-  before_action :set_vendor_id, only: [:create, :update]
-end
